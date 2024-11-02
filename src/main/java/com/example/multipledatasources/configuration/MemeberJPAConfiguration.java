6a0facfaaadab137b6206d11dec610d52006f668
Raja Kolli <rajadilipkolli@gmail.com>
2024-11-02 08:08:09 +0000

    fix : Issue with entity scan

--- src/main/java/com/example/multipledatasources/configuration/MemeberJPAConfiguration.java
+++ src/main/java/com/example/multipledatasources/configuration/MemeberJPAConfiguration.java
@@ -1,9 +1,12 @@
 package com.example.multipledatasources.configuration;
 
+import com.example.multipledatasources.model.member.Member;
 import com.example.multipledatasources.repository.member.MemberRepository;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration(proxyBeanMethods = false)
+@EntityScan(basePackageClasses = Member.class)
 @EnableJpaRepositories(basePackageClasses = MemberRepository.class, entityManagerFactoryRef = "entityManagerFactory")
 class MemeberJPAConfiguration {}
